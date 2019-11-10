bd37ee37790af7d0aa30608bd61ce58e9448c623
smakesoska1 <smakesoska1@etf.unsa.ba>
2019-11-10 23:10:37 +0100

    incijalizacija promjenjivih u klasi Board

--- src/ba/unsa/etf/rpr/Board.java
+++ src/ba/unsa/etf/rpr/Board.java
@@ -1,5 +1,18 @@
 package ba.unsa.etf.rpr;
 
 public class Board {
+public ChessPiece.Color color;
+private char letter;
+private char number;
+
+public Field[][] fields=new Field[8][8];
+public ChessPiece[]chessPieces=new ChessPiece[32];
+
+public void setColor(ChessPiece.Color color){
+    this.color=color;
+}
+
+
+
 
 }
