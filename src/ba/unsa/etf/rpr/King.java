4617899052e06073bd0c6ac381f06d28e0c0f57f
smakesoska1 <smakesoska1@etf.unsa.ba>
2019-11-11 00:07:05 +0100

    kreiranje prototipa nekih metoda u klasama King i Pawn

--- src/ba/unsa/etf/rpr/King.java
+++ src/ba/unsa/etf/rpr/King.java
@@ -1,4 +1,9 @@
 package ba.unsa.etf.rpr;
 
 public class King {
+    public King(String e1, ChessPiece.Color white) {
+    }
+
+    public void move(String d2) {
+    }
 }
--- src/ba/unsa/etf/rpr/Pawn.java
+++ src/ba/unsa/etf/rpr/Pawn.java
@@ -1,4 +1,27 @@
 package ba.unsa.etf.rpr;
 
-public class Pawn {
+public class Pawn extends ChessPiece {
+    public Pawn(){}
+    public Pawn( String color, Color white) {
+    }
+
+    @Override
+    public String getPosition() {
+        return null;
+    }
+
+    @Override
+    public boolean move(String position) throws IllegalChessMoveException {
+        return false;
+    }
+
+    @Override
+    public void setColor(Color color) {
+
+    }
+
+    @Override
+    public void setPosition(String position) {
+
+    }
 }
