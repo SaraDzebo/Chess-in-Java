5a795e0fc26bc3a1c60a608c2b9e25338e134de5
smakesoska1 <smakesoska1@etf.unsa.ba>
2019-11-10 23:19:01 +0100

    kreiranje konstruktora za klasu Board

--- src/ba/unsa/etf/rpr/Board.java
+++ src/ba/unsa/etf/rpr/Board.java
@@ -1,18 +1,29 @@
 package ba.unsa.etf.rpr;
 
 public class Board {
-public ChessPiece.Color color;
-private char letter;
-private char number;
+    public ChessPiece.Color color;
+    private char letter;
+    private char number;
 
-public Field[][] fields=new Field[8][8];
-public ChessPiece[]chessPieces=new ChessPiece[32];
+    public Field[][] fields = new Field[8][8];
+    public ChessPiece[] chessPieces = new ChessPiece[32];
 
-public void setColor(ChessPiece.Color color){
-    this.color=color;
-}
+    public void setColor(ChessPiece.Color color) {
+        this.color = color;
+    }
 
+    public ChessPiece.Color getColor() {
+        return color;
+    }
 
-
-
+    //konstruktorza klasu Board pocetno stanje
+    public Board() {
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                fields[i][j] = new Field();
+                //postavljanje na pocento
+                fields[i][j].setIs_free(true);
+            }
+        }
+    }
 }
