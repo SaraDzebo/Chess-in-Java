7c8c6ebd203499bc2012bab7101cc2efd773d979
smakesoska1 <smakesoska1@etf.unsa.ba>
2019-11-11 00:06:06 +0100

    kreiranje i status polja za pijune bijele boje i kreiranje prototipa metoda move u klasi Board

--- src/ba/unsa/etf/rpr/Board.java
+++ src/ba/unsa/etf/rpr/Board.java
@@ -3,7 +3,7 @@ package ba.unsa.etf.rpr;
 public class Board {
     public ChessPiece.Color color;
     private char letter;
-    private char number;
+    private String number;
 
     public Field[][] fields = new Field[8][8];
     public ChessPiece[] chessPieces = new ChessPiece[32];
@@ -25,5 +25,31 @@ public class Board {
                 fields[i][j].setIs_free(true);
             }
         }
+
+
+        //rucno unosenje figura-za bijele
+
+        for (int i=0; i < 8; i++) {
+            chessPieces[i] = new Pawn();
+            chessPieces[i].setColor(ChessPiece.Color.WHITE);
+            number= "2";
+            letter= (char) ('A'+i);
+
+            number=String.valueOf(letter)+number;
+            chessPieces[i].setPosition(number);
+            fields[6][0].setIs_free(false);
+            fields[6][i].setPosition(number);
+        }
+    }
+
+
+    public boolean isCheck(ChessPiece.Color white) {
+        return true;
+    }
+
+    public void move(String e2, String e4) {
+    }
+
+    public void move(Class<Pawn> pawnClass, ChessPiece.Color white, String e4) {
     }
 }
