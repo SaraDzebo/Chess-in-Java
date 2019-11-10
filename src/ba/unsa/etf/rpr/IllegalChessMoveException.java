a735b026b74f397b5734f4c07edcc689cd0962d6
smakesoska1 <smakesoska1@etf.unsa.ba>
2019-11-10 16:40:41 +0100

    dodani kontruksotir u klasu IllegalChessMoveException

--- src/ba/unsa/etf/rpr/IllegalChessMoveException.java
+++ src/ba/unsa/etf/rpr/IllegalChessMoveException.java
@@ -1,4 +1,19 @@
 package ba.unsa.etf.rpr;
 
 public class IllegalChessMoveException extends Exception {
+    public boolean have_error=false;
+
+    public IllegalChessMoveException(){
+
+    }
+
+    public IllegalChessMoveException(String message){
+    this.have_error=true;
+    }
+
+    public IllegalChessMoveException(String message,Throwable throwable){
+        this.have_error=true;
+    }
+
+
 }
--- src/ba/unsa/etf/rpr/Knight.java
+++ src/ba/unsa/etf/rpr/Knight.java
@@ -1,6 +1,9 @@
 package ba.unsa.etf.rpr;
 
 public class Knight {
+    public Knight(String b1, ChessPiece.Color white) {
+    }
+
     public void move(String c3) {
     }
 }
