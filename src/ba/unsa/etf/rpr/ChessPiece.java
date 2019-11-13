1b53c3bf030eed2b659130e75c5a86e13fe81ac7
smakesoska1 <smakesoska1@etf.unsa.ba>
2019-11-13 23:27:28 +0100

    dodana pomocna funkcija u klasi ChessPiece

--- src/ba/unsa/etf/rpr/ChessPiece.java
+++ src/ba/unsa/etf/rpr/ChessPiece.java
@@ -2,7 +2,7 @@ package ba.unsa.etf.rpr;
 
 public abstract class ChessPiece {
     protected Color color;
-    public static enum Color{BLACK,WHITE}
+    public  enum Color{BLACK,WHITE}
     protected String position;
 
     public ChessPiece(){} //konstruktor bez parametara
@@ -10,6 +10,44 @@ public abstract class ChessPiece {
     public ChessPiece(Color color,String position){
       this.color=color;
       this.position=position;
+    }
+
+    public int helpFunction(char znak){
+        int y=0;
+        if(znak=='A')
+            y=0;
+        if(znak=='B')
+            y=1;
+        if(znak=='C')
+            y=2;
+        if(znak=='D')
+            y=3;
+        if(znak=='E')
+            y=4;
+        if(znak=='F')
+            y=5;
+        if(znak=='G')
+            y=6;
+        if(znak=='H')
+            y=7;
+        if(znak=='8')
+            y=0;
+        if(znak=='7')
+            y=1;
+        if(znak=='6')
+            y=2;
+        if(znak=='5')
+            y=3;
+        if(znak=='4')
+            y=4;
+        if(znak=='3')
+            y=5;
+        if(znak=='2')
+            y=6;
+        if(znak=='1')
+            y=7;
+        return y;
+
     }
 
     public abstract Color getColor();
