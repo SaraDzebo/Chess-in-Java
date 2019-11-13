ec0e457b7b763fed0f2231776c773f7336b659d8
smakesoska1 <smakesoska1@etf.unsa.ba>
2019-11-14 00:30:26 +0100

    stubovi metoda i klasama

--- src/ba/unsa/etf/rpr/Bishop.java
+++ src/ba/unsa/etf/rpr/Bishop.java
@@ -1,4 +1,18 @@
 package ba.unsa.etf.rpr;
 
-public class Bishop {
+public class Bishop extends ChessPiece {
+    @Override
+    public Color getColor() {
+        return null;
+    }
+
+    @Override
+    public String getPosition() {
+        return null;
+    }
+
+    @Override
+    public void move(String position) throws IllegalChessMoveException {
+
+    }
 }
--- src/ba/unsa/etf/rpr/Knight.java
+++ src/ba/unsa/etf/rpr/Knight.java
@@ -1,7 +1,17 @@
 package ba.unsa.etf.rpr;
 
-public class Knight {
+public class Knight extends ChessPiece{
     public Knight(String b1, ChessPiece.Color white) {
+    }
+
+    @Override
+    public Color getColor() {
+        return null;
+    }
+
+    @Override
+    public String getPosition() {
+        return null;
     }
 
     public void move(String c3) {
