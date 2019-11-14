187ebaa77d90b19c6e1f68917287ba2c11658e70
smakesoska1 <smakesoska1@etf.unsa.ba>
2019-11-14 12:14:57 +0100

    implementirana metoda move u klasi King/ padaju dva testa

--- src/ba/unsa/etf/rpr/King.java
+++ src/ba/unsa/etf/rpr/King.java
@@ -16,16 +16,39 @@ public class King extends ChessPiece{
     }
 
 
+
     @Override
     public Color getColor() {
-        return null;
+        return boja;
     }
 
     @Override
     public String getPosition() {
-        return null;
+        return pozicija;
+    }
+    public void setPozicija(String pozicija){
+        this.pozicija=pozicija;
     }
 
-    public void move(String d2) {
+    public void move(String position) throws IllegalChessMoveException {
+        //iz konstruktora
+
+        if(position.length()!=2) throw new IllegalArgumentException("Pozicija nije zadana u ispravnom formatu");
+        if((!((position.charAt(0)>=65 && position.charAt(0)<=72)|| (position.charAt(0)>=97 && position.charAt(0)<=104)))||
+                (!(position.charAt(1)>=49 && position.charAt(1)<=56))) throw new IllegalArgumentException("Pozicija za kralja se nalazi van sahovske table");
+
+        //kralj se moze pomjerati smao jedno mjesto:naprijed , nazad, sa strana i dijagonalno
+
+        boolean pomjeranje = false;
+        if (pozicija.charAt(0) == position.charAt(0) && ((position.charAt(0) + 1) == (pozicija.charAt(0) + 1) || (position.charAt(1)-1)==pozicija.charAt(1)))
+            pomjeranje = true;
+
+        if ((position.charAt(0) == (pozicija.charAt(0) + 1) && position.charAt(1) == (pozicija.charAt(1) + 1)) || (position.charAt(0) == (pozicija.charAt(0) - 1) && position.charAt(1) == (pozicija.charAt(1) - 1))
+                || (position.charAt(0) == (pozicija.charAt(0) + 1) && position.charAt(1) == (pozicija.charAt(1) - 1)) || (position.charAt(0) == (pozicija.charAt(0) - 1) && position.charAt(1) == (pozicija.charAt(1) + 1)))
+            pomjeranje = true;
+        if (pozicija.charAt(1) == position.charAt(1) && ((position.charAt(0) + 1) == pozicija.charAt(0) || (position.charAt(0) - 1) == pozicija.charAt(0)))
+            pomjeranje = true;
+        if (!pomjeranje) throw new IllegalChessMoveException("Ovaj potez nije dozvoljen za kralja!");
+        this.setPozicija(position);
     }
-}
+    }
