a7656fc5a70aa681a72880510605681d0923d3cc
smakesoska1 <smakesoska1@etf.unsa.ba>
2019-11-14 12:32:05 +0100

    zavrsena implementacija metode move u klasi Rook

--- src/ba/unsa/etf/rpr/Rook.java
+++ src/ba/unsa/etf/rpr/Rook.java
@@ -40,6 +40,7 @@ public class Rook extends ChessPiece {
 
         if(position.charAt(0)!=this.getPosition().charAt(0) && position.charAt(1)!=this.getPosition().charAt(1)) throw new IllegalChessMoveException("Potez nije dozvoljen");
         //ispitujemo da li se preskacu neke figure prilikom pomjeranja
+        //top se moze kretati naprijed, nazad, sa strane koliko god mjesta ali bez preskakanja figura
 
 
             if(position.charAt(1)<pozicija.charAt(1)){
@@ -56,16 +57,18 @@ public class Rook extends ChessPiece {
                 }
             }
         }
-            if(helpFunction(position.charAt(0))>helpFunction(pozicija.charAt(0))){
-                for(int i=helpFunction(pozicija.charAt(0))+1;i<helpFunction(position.charAt(0));i++){
-                    if(Board.fields[helpFunction(position.charAt(1))][i]!=null)throw new IllegalChessMoveException("Nije dozvoljeno preskakanje figura za pijun top!");
-                }
-            }
+
         if(position.charAt(0)==pozicija.charAt(0)){
             if(position.charAt(1)>pozicija.charAt(1)){
                 for(int i=helpFunction(position.charAt(1))+1;i<helpFunction(pozicija.charAt(1));i++){
                     if(Board.fields[i][helpFunction(position.charAt(0))]!=null)throw new IllegalChessMoveException("Nije dozvoljeno preskakanje figura za pijun top!");
                 }
+            }
+        }
+
+        if(helpFunction(position.charAt(0))>helpFunction(pozicija.charAt(0))){
+            for(int i=helpFunction(pozicija.charAt(0))+1;i<helpFunction(position.charAt(0));i++){
+                if(Board.fields[helpFunction(position.charAt(1))][i]!=null)throw new IllegalChessMoveException("Nije dozvoljeno preskakanje figura za pijun top!");
             }
         }
         this.setPozicija(position);
