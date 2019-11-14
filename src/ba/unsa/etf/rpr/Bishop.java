a2970aceaa8d9d7c8115cdec0b89e5d35d4f729c
smakesoska1 <smakesoska1@etf.unsa.ba>
2019-11-14 12:43:43 +0100

    implementacija konstruktora,getera i setera u klasi Bishop/sve osim move metode

--- src/ba/unsa/etf/rpr/Bishop.java
+++ src/ba/unsa/etf/rpr/Bishop.java
@@ -1,21 +1,43 @@
 package ba.unsa.etf.rpr;
 
 public class Bishop extends ChessPiece {
-    public Bishop(String c8, Color black) {
+    private String pozicija;
+    private Color boja;
+
+    public Bishop(String pozicija, Color boja) {
+        if (pozicija.length() != 2) {
+            throw new IllegalArgumentException("Pozicija nije u ispravnom formatu.");
+        }
+        if (!(pozicija.charAt(0) >= 65 && pozicija.charAt(0) <= 72) || (pozicija.charAt(0) >= 97 && pozicija.charAt(0) <= 104) || !(pozicija.charAt(1) <= 49 && pozicija.charAt(1) <= 56)) {
+            throw new IllegalArgumentException("Pozicija se nalazi van sahovske table.");
+        }
+        this.pozicija = pozicija;
+        this.boja = boja;
     }
+
 
     @Override
     public Color getColor() {
-        return null;
+        return boja;
     }
 
     @Override
     public String getPosition() {
-        return null;
+        return pozicija;
+    }
+
+    public void setPozicija(String pozicija) {
+        this.pozicija = pozicija;
     }
 
     @Override
     public void move(String position) throws IllegalChessMoveException {
+        if (pozicija.length() != 2) {
+            throw new IllegalArgumentException("Pozicija nije u ispravnom formatu.");
+        }
+        if (!(pozicija.charAt(0) >= 65 && pozicija.charAt(0) <= 72) || (pozicija.charAt(0) >= 97 && pozicija.charAt(0) <= 104) || !(pozicija.charAt(1) <= 49 && pozicija.charAt(1) <= 56)) {
+            throw new IllegalArgumentException("Pozicija se nalazi van sahovske table.");
+        }
 
     }
 }
