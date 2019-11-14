1fed7c15465c69755c0062f0b15c1e24d100869a
smakesoska1 <smakesoska1@etf.unsa.ba>
2019-11-14 14:05:52 +0100

    zapoceta implementacija metode move u klasi Pawn

--- src/ba/unsa/etf/rpr/Pawn.java
+++ src/ba/unsa/etf/rpr/Pawn.java
@@ -1,15 +1,15 @@
 package ba.unsa.etf.rpr;
 
 public class Pawn extends ChessPiece {
-    private String position;
+    private String pozicija;
     private Color color;
-    public Pawn(String position,Color color){
-        this.position=position;
+    public Pawn(String pozicija,Color color){
+        this.pozicija=pozicija;
         this.color=color;
-        if(position.length()!=2) {
+        if(pozicija.length()!=2) {
             throw new IllegalArgumentException("Pozicija nije u ispravnom formatu.");
         }
-        if(!(position.charAt(0)>=65 && position.charAt(0)<=72) || (position.charAt(0)>=97 && position.charAt(0)<=104)){
+        if(!(pozicija.charAt(0)>=65 && pozicija.charAt(0)<=72) || (pozicija.charAt(0)>=97 && pozicija.charAt(0)<=104)){
 throw new IllegalArgumentException("Pozicija se nalazi an tabele.");
         }
     }
@@ -18,16 +18,25 @@ throw new IllegalArgumentException("Pozicija se nalazi an tabele.");
         return color;
     }
     public void setPozicija(String pozicija){
-        this.position=pozicija;
+        this.pozicija=pozicija;
     }
 
     @Override
-    public String getPosition() {
-        return null;
+    public String getPosition()
+    {
+        return pozicija;
     }
 
     @Override
     public void move(String position) throws IllegalChessMoveException {
+        if(pozicija.length()!=2) {
+            throw new IllegalArgumentException("Pozicija nije u ispravnom formatu.");
+        }
+        if(!(pozicija.charAt(0)>=65 && pozicija.charAt(0)<=72) || (pozicija.charAt(0)>=97 && pozicija.charAt(0)<=104)){
+            throw new IllegalArgumentException("Pozicija se nalazi van tabele.");
+        }
+
 
     }
-}
+    }
+
