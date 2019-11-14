8477088525fbad2ab2b4c31eacfe412021359738
smakesoska1 <smakesoska1@etf.unsa.ba>
2019-11-14 11:53:39 +0100

    kostruktor u klasi King

--- src/ba/unsa/etf/rpr/King.java
+++ src/ba/unsa/etf/rpr/King.java
@@ -1,8 +1,20 @@
 package ba.unsa.etf.rpr;
 
 public class King extends ChessPiece{
-    public King(String e1, ChessPiece.Color white) {
+    private String pozicija;
+    private Color boja;
+    //konsturktor klase King
+    public King(String pozicija, Color boja) {
+        if(pozicija.length()!=2) {
+            throw new IllegalArgumentException("Pozicija nije u ispravnom formatu.");
+        }
+        if(!(pozicija.charAt(0)>=65 && pozicija.charAt(0)<=72) || (pozicija.charAt(0)>=97 && pozicija.charAt(0)<=104) || !(pozicija.charAt(1)<=49 && pozicija.charAt(1)<=56)){
+            throw new IllegalArgumentException("Pozicija se nalazi an tabele.");
+        }
+        this.pozicija=pozicija;
+        this.boja=boja;
     }
+
 
     @Override
     public Color getColor() {
